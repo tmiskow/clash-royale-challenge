f3cd14ecc23f30b5d4a13a4e7d0cfdb5996a8523
Mateusz Olko <mateusz.olko@gmail.com>
2019-06-10 11:04:59 +0200

    Fix bug with original validation

--- src/genetic.py
+++ src/genetic.py
@@ -283,8 +283,12 @@ def resample_validation(train_data:DataSet, valid_data: DataSet, train_for_valid
 def run_evolution(train_data: DataSet, valid_data: DataSet, pool: mp.Pool, params: EvolutionParams):
     if params.validation_mode == "original":
         valid_index = sample(params.n_valid_samples, valid_data.ids)
+        new_valid_data = valid_data
     elif params.validation_mode == "upsampling":
         train_for_validation_ids = train_data.ids[params.weights > params.weights.mean()]
+        new_valid_data, valid_index = resample_validation(train_data, valid_data, train_for_validation_ids,
+                                                          params.n_valid_samples)
+
     else:
         raise AttributeError(f"Validation mode {params.validation_mode} does not exists")
 
@@ -299,7 +303,6 @@ def run_evolution(train_data: DataSet, valid_data: DataSet, pool: mp.Pool, param
     else:
         train_ids = params.train_ids
 
-    new_valid_data, valid_index = resample_validation(train_data, valid_data, train_for_validation_ids, params.n_valid_samples)
     next_gen_params = GenerationParams(
         n_models=params.n_models,
         n_fits=params.n_fits,
